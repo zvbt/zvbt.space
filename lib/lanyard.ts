53b08c1a320e80c3364b4917cad997ca79df3ab4
zvbt <cyadine@pm.me>
2024-07-08 14:30:52 +0200

    Using self hosted lanyard api

--- lib/lanyard.ts
+++ lib/lanyard.ts
@@ -1,10 +1,9 @@
 import { LanyardResponse} from "@/types/lanyard";
 const ID = process.env.DISCORD_ID;
 
-
 export async function lanyard() {
   const resp = await fetch(
-    `https://api.lanyard.rest/v1/users/${ID}`,
+    `https://lanyard.zvbt.space/v1/users/${ID}`,
     {
       cache: "no-cache",
     }
