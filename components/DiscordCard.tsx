de696f86639d20c48d6322e596642f644e81468d
zvbt <cyadine@pm.me>
2024-07-29 09:33:33 +0200

    Fixed online bubble

--- components/DiscordCard.tsx
+++ components/DiscordCard.tsx
@@ -38,7 +38,7 @@ const DiscordCard: React.FC<DiscordCardProps> = ({
             className="w-90 sm:w-96 z-10 rounded-tr-lg p-2 sm:p-5 rounded-lg">
                 <div className="flex flex-col">
                     <div className="flex items-center space-x-2">
-                    <div className={cn("",status === "dnd" && "fixed w-4 h-4 rounded-full ml-[50px] mt-[48px] bg-[#e78284] border-2 border-black",status === "online" && "fixed w-4 h-4 rounded-full ml-[50px] mt-[48px] bg-[#a6d189] border-2 border-black",status === "idle" && "fixed w-4 h-4 rounded-full ml-[50px] mt-[48px] bg-[#e5c890] border-2 border-black",status === "offline" && "fixed w-4 h-4 rounded-full ml-[50px] mt-[48px] bg-[#737994] border-2 border-black")}></div>
+                    <div className={cn("",status === "dnd" && "fixed w-4 h-4 rounded-full ml-[50px] mt-[48px] bg-[#e78284] border-2 border-black",status === "online" && "absolute w-4 h-4 rounded-full ml-[50px] mt-[48px] bg-[#a6d189] border-2 border-black",status === "idle" && "fixed w-4 h-4 rounded-full ml-[50px] mt-[48px] bg-[#e5c890] border-2 border-black",status === "offline" && "fixed w-4 h-4 rounded-full ml-[50px] mt-[48px] bg-[#737994] border-2 border-black")}></div>
                     <a href={link} target="_blank" className="fixed w-[64px] h-[64px] rounded-full">
                     <div className={'fixed w-[64px] h-[64px] rounded-full opacity-0 hover:opacity-100 hover:bg-[#11111bc2] flex items-center justify-center'}>
                      <Icons.external className="w-5 h-5"/> 
