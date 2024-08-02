1e32237df80826ece6c4b79b8271a3c0c19824e0
zvbt <cyadine@pm.me>
2024-08-02 20:34:35 +0200

    Searchbox url handling

--- app/startpage/page.tsx
+++ app/startpage/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { useEffect, useRef } from 'react';
+import { FormEvent, useEffect, useRef } from 'react';
 import Image from "next/image";
 import NoteTaking from "@/components/NoteTaking";
 import Clock from "@/components/clock";
@@ -8,10 +8,24 @@ export default function Home() {
     const inputRef = useRef<HTMLInputElement>(null);
 
     useEffect(() => {
-        if (inputRef.current) {
-            inputRef.current.focus();
-        }
+        inputRef.current?.focus();
     }, []);
+
+    const handleSubmit = (event: FormEvent) => {
+        event.preventDefault();
+        const query = inputRef.current?.value;
+        if (!query) return;
+
+        const urlPattern = /^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([/\w .-]*)*\/?$/;
+        
+        if (urlPattern.test(query)) {
+            const url = query.startsWith('http') ? query : `http://${query}`;
+            window.location.href = url;
+        } else {
+            const searchUrl = `https://www.google.com/search?q=${encodeURIComponent(query)}`;
+            window.location.href = searchUrl;
+        }
+    };
 
     return (
         <main>
@@ -19,7 +33,7 @@ export default function Home() {
             <Image src={'https://r2.e-z.host/7ed0180f-b228-49a7-be1e-0183c1938777/a8hokk7w.png'} width={1920} height={1080} className='fixed object-cover w-full h-full blur-sm z-0' draggable={false} alt='bg' quality={100}/>
             <Clock />          
             <div className="flex justify-center items-center h-screen z-10 relative">
-                <form action="https://www.google.com/search" method="get" className="w-full max-w-md">
+                <form onSubmit={handleSubmit} className="w-full max-w-md">
                     <input 
                         type="text" 
                         name="q" 
