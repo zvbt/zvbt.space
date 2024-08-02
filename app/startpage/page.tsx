a6b98bc1c9064ea96742d3cef171fae7d73392ae
zvbt <cyadine@pm.me>
2024-08-02 20:25:54 +0200

    Auto focus search box + clock

--- app/startpage/page.tsx
+++ app/startpage/page.tsx
@@ -1,25 +1,35 @@
 'use client'
+import { useEffect, useRef } from 'react';
 import Image from "next/image";
-import Link from "next/link";
 import NoteTaking from "@/components/NoteTaking";
+import Clock from "@/components/clock";
 
-export default async function Home() {
+export default function Home() {
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    useEffect(() => {
+        if (inputRef.current) {
+            inputRef.current.focus();
+        }
+    }, []);
+
     return (
         <main>
             <title>New Tab</title>
-             <Image src={'https://r2.e-z.host/7ed0180f-b228-49a7-be1e-0183c1938777/a8hokk7w.png'} width={1920} height={1080} className='fixed object-cover w-full h-full blur-sm z-0' draggable={false} alt='bg' quality={100}/>            
+            <Image src={'https://r2.e-z.host/7ed0180f-b228-49a7-be1e-0183c1938777/a8hokk7w.png'} width={1920} height={1080} className='fixed object-cover w-full h-full blur-sm z-0' draggable={false} alt='bg' quality={100}/>
+            <Clock />          
             <div className="flex justify-center items-center h-screen z-10 relative">
                 <form action="https://www.google.com/search" method="get" className="w-full max-w-md">
                     <input 
                         type="text" 
                         name="q" 
                         placeholder="서치" 
+                        ref={inputRef}
                         className="text-center w-full px-4 py-2 border rounded-full shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 bg-white bg-opacity-10 backdrop-filter backdrop-blur-lg text-white placeholder-white"
                     />
                 </form>
             </div>
             <NoteTaking />
-            
         </main>
     );
 }
