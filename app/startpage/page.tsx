cd9c4408300edad2bbc6e3ebef44ba6cfd292cb5
zvbt <cyadine@pm.me>
2024-08-25 23:23:13 +0200

    Fix formatting

--- app/startpage/page.tsx
+++ app/startpage/page.tsx
@@ -19,7 +19,6 @@ export default function Home() {
     }, []);
 
     useEffect(() => {
-        // Fetch the bangs JSON file
         fetch('/bangs.json')
             .then(response => response.json())
             .then(data => setBangs(data))
@@ -27,30 +26,34 @@ export default function Home() {
     }, []);
 
     const handleSubmit = (event: FormEvent) => {
-        event.preventDefault();
-        const query = inputRef.current?.value;
-        if (!query) return;
+    event.preventDefault();
+    const query = inputRef.current?.value;
+    if (!query) return;
 
-        const urlPattern = /^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([/\w .-]*)*\/?$/;
+    const urlPattern = /^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([/\w .-]*)*\/?$/;
 
-        if (urlPattern.test(query)) {
-            const url = query.startsWith('http') ? query : `http://${query}`;
-            window.location.href = url;
+    if (urlPattern.test(query)) {
+        const url = query.startsWith('http') ? query : `http://${query}`;
+        window.location.href = url;
+    } else {
+        const bangKey = Object.keys(bangs)
+            .sort((a, b) => b.length - a.length)
+            .find(bang => query.startsWith(bang));
+
+        if (bangKey) {
+            const bangQuery = query.replace(bangKey, '').trim();
+            const { baseUrl, searchPattern } = bangs[bangKey];
+            const searchUrl = bangQuery
+                ? `${baseUrl}/${searchPattern}${encodeURIComponent(bangQuery)}`
+                : baseUrl;
+            window.location.href = searchUrl;
         } else {
-            const bangKey = Object.keys(bangs).find(bang => query.startsWith(bang));
-            if (bangKey) {
-                const bangQuery = query.replace(bangKey, '').trim();
-                const { baseUrl, searchPattern } = bangs[bangKey];
-                const searchUrl = bangQuery
-                    ? `${baseUrl}/${searchPattern}${encodeURIComponent(bangQuery)}`
-                    : baseUrl;
-                window.location.href = searchUrl;
-            } else {
-                const searchUrl = `https://www.google.com/search?q=${encodeURIComponent(query)}`;
-                window.location.href = searchUrl;
-            }
+            const searchUrl = `https://www.google.com/search?q=${encodeURIComponent(query)}`;
+            window.location.href = searchUrl;
         }
-    };
+    }
+};
+
 
     const [currentImage, setCurrentImage] = useState('/img/side1.gif');
     const [imageKey, setImageKey] = useState(0); // force re-render of image
