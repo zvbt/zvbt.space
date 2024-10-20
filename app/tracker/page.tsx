bbb514f597120be58505607fc17d2c4a36013a86
zvbt <cyadine@pm.me>
2024-10-20 13:14:31 +0200

    Update page.tsx

--- app/tracker/page.tsx
+++ app/tracker/page.tsx
@@ -2,6 +2,7 @@
 import { useEffect, useState, FormEvent, useRef } from 'react';
 import Image from "next/image";
 import LeagueStats from '@/components/LeagueStats';
+import ValorantStats from '@/components/ValStats';
 
 
 export default function Home() {
@@ -9,9 +10,24 @@ export default function Home() {
         <main>
             <Image src={'https://r2.e-z.host/7ed0180f-b228-49a7-be1e-0183c1938777/a8hokk7w.png'} width={1920} height={1080} className='fixed object-cover w-full h-full blur-sm z-0' draggable={false} alt='bg' quality={100}/>
 
-            <div className="min-h-screen flex items-center justify-center absolute">
-             
-            <LeagueStats />
+            <div className="flex absolute space-x-2 m-4">
+                <div>
+                    <p className='max-w-lg p-2 bg-[#11111bc2] text-white rounded-lg shadow-md flex'>League Of Legends</p>
+                    <LeagueStats />
+                </div>
+                <div>
+                    <p className='max-w-lg p-2 bg-[#11111bc2] text-white rounded-lg shadow-md flex'>Valorant</p>
+                    <ValorantStats />
+                </div>
+                <div>
+                    <p className='max-w-lg p-2 bg-[#11111bc2] text-white rounded-lg shadow-md flex'>n/a</p>
+                    <ValorantStats />
+                </div>
+                <div>
+                    <p className='max-w-lg p-2 bg-[#11111bc2] text-white rounded-lg shadow-md flex'>n/a</p>
+                    <ValorantStats />
+                </div>
+            
             </div>
         </main>
     );
