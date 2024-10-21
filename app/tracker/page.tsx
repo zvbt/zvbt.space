8fe48d3d2a0b916968721f5561c6fdf4704e1109
zvbt <cyadine@pm.me>
2024-10-21 19:31:15 +0200

    Update page.tsx

--- app/tracker/page.tsx
+++ app/tracker/page.tsx
@@ -8,26 +8,18 @@ import ValorantStats from '@/components/ValStats';
 export default function Home() {
     return (
         <main>
-            <Image src={'https://r2.e-z.host/7ed0180f-b228-49a7-be1e-0183c1938777/a8hokk7w.png'} width={1920} height={1080} className='fixed object-cover w-full h-full blur-sm z-0' draggable={false} alt='bg' quality={100}/>
+            <Image src={'https://r2.e-z.host/7ed0180f-b228-49a7-be1e-0183c1938777/fhaewi1i.png'} width={1920} height={1080} className='fixed object-cover w-full h-full blur-sm z-0' draggable={false} alt='bg' quality={100}/>
 
             <div className="flex absolute space-x-2 m-4">
                 <div>
                     <p className='max-w-lg p-2 bg-[#11111bc2] text-white rounded-lg shadow-md flex'>League Of Legends</p>
                     <LeagueStats />
                 </div>
-                <div>
-                    <p className='max-w-lg p-2 bg-[#11111bc2] text-white rounded-lg shadow-md flex'>Valorant</p>
+                {/* <div>
+                    <p className='max-w-lg p-2 bg-[#11111bc2] text-white rounded-lg shadow-md flex'>Valorant (SOON)</p>
                     <ValorantStats />
-                </div>
-                <div>
-                    <p className='max-w-lg p-2 bg-[#11111bc2] text-white rounded-lg shadow-md flex'>n/a</p>
-                    <ValorantStats />
-                </div>
-                <div>
-                    <p className='max-w-lg p-2 bg-[#11111bc2] text-white rounded-lg shadow-md flex'>n/a</p>
-                    <ValorantStats />
-                </div>
-            
+                </div> */}
+
             </div>
         </main>
     );
