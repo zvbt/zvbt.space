c5399d3aadbfd5a0c32c4b44eb477020f3116937
zvbt <cyadine@pm.me>
2024-10-26 14:07:27 +0200

    Added favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <link rel="shortcut icon" href="planet.png" type="image/x-icon" />
       <body className={karla.className}>{children}</body>
     </html>
   );
