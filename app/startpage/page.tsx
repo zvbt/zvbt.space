576b550a7f97e30674ac59becc0f47312103b03e
zvbt <cyadine@pm.me>
2025-01-06 10:48:35 +0100

    Update player link

--- app/startpage/page.tsx
+++ app/startpage/page.tsx
@@ -90,7 +90,7 @@ export default function Home() {
     return (
         <main>
             <div className="fixed bottom-2 left-0 right-0 z-50 ml-2">
-                <AudioPlayer src="http://192.168.1.40:8001/kpop.mp3" />
+                <AudioPlayer src="https://stream.zvbt.cc/kpop.mp3" />
             </div>
             <body className='font-iosevka text-[20px] w-[670px] h-[320px] absolute top-0 bottom-[100px] left-0 right-0 m-auto bg-[#0F0F12]'>
             <title>New Tab</title>
